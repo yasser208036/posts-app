c9a0a1b107ed2b49da1d459f15ed13c617ec7c44
Yasser <yassermohamed654321@gmail.com>
2026-07-23 17:49:33 +0300

    chore: configure Vercel deployment, production environment variables, and Prisma deployment scripts

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiUrl: "https://YOUR_RENDER_BACKEND_URL/api",
+  googleClientId:
+    "344849653228-fet3vsmp85r9hml5uat66esn2gb7pvft.apps.googleusercontent.com",
+};
